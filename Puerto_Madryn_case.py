5be252dac8cc48af34437235e17d1bb355997be2
Andrew Woodill <johnwoodill@gmail.com>
2019-03-21 19:22:46 -0700

    animating map

--- Puerto_Madryn_case.py
+++ Puerto_Madryn_case.py
@@ -1,6 +1,13 @@
 import pandas as pd
-import numpy as np
-import glob
+import numpy as np 
+import feather
+import os as os
+#os.environ["PROJ_LIB"] = "/Users/john/miniconda3/share/proj"; #fixr
+#from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+#import glob
+%matplotlib inline
+import cartopy.crs as ccrs
 
 def spherical_dist_populate(data=None, lat_lis=None, lon_lis=None, r=3958.75):
     
@@ -28,9 +35,8 @@ def spherical_dist_populate(data=None, lat_lis=None, lon_lis=None, r=3958.75):
 
     mtx = r * np.arccos(cos_lat_d - cos_lat_i*cos_lat_J*(1 - cos_lon_d))
     
+    # Build data.frame
     matdat = pd.DataFrame(mtx)
-    #matdat = matdat.rename(index=mmsi[:], columns=mmsi[:])    
-
     matdat.columns = mmsi[:]
     matdat = matdat.set_index(mmsi[:])
     
@@ -70,17 +76,18 @@ def spherical_dist_populate(data=None, lat_lis=None, lon_lis=None, r=3958.75):
 
 def interp_hr(data):
     
+    indat = data
     # Sort data by timestamp
     data = data.sort_values('timestamp')
+    data['timestamp'] = data['timestamp'].dt.round('min')
+    data = data.groupby('timestamp', as_index=False)[['lat', 'lon']].agg('mean')
     
+    # Begin and end date data frame
     start = pd.Timestamp(data['timestamp'].iat[0]).strftime('%m-%d-%Y %H:%M')
     end = pd.Timestamp(data['timestamp'].iat[-1]).strftime('%m-%d-%Y %H:%M')
-
+    
     # Merge and interpolate between start and end
     pdat = pd.DataFrame({'timestamp': pd.date_range(start=start, end=end, freq='min')})
-    data['timestamp'] = data['timestamp'].dt.round('min')
-
-    data = data.groupby('timestamp', as_index=False)[['lat', 'lon']].agg('mean')
     
     pdat = pdat.merge(data, on='timestamp', how='left')
     pdat['lat'] = pd.Series(pdat['lat']).interpolate()
@@ -141,16 +148,21 @@ print(mdat)
 mdat = mdat.reset_index(drop=True)
 mdat.to_feather('~/Data/GFW_point/Patagonia_Shelf/complete/Puerto_Madryn_2016-03-10_2016-03-20.feather')
 
+mdat = pd.read_feather('~/Data/GFW_point/Patagonia_Shelf/complete/Puerto_Madryn_2016-03-10_2016-03-20.feather')
+
 # Patagonia shelf
 lon1 = -77
 lon2 = -22
 lat1 = -58
 lat2 = -23
 
+#
 mlon1 = -65.925222
 mlon2 = -54.418267
 mlat1 = -45.145745
 mlat2 = -38.147991
+
+
 
 my_dpi=96
 fig = plt.figure(figsize=(2600/my_dpi, 1800/my_dpi), dpi=my_dpi)
@@ -170,13 +182,14 @@ ax.plot(mlon2, mlat1, 'o', markersize=10, color = 'red', transform=ccrs.PlateCar
 my_dpi=96
 fig = plt.figure(figsize=(2600/my_dpi, 1800/my_dpi), dpi=my_dpi)
 ax = plt.axes(projection=ccrs.PlateCarree())
+#ax.background_img(name='BM', resolution='low')
 ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
 ax.coastlines()
 #ax.stock_img()
 
 
-tdat = mdat[mdat['timestamp'] == "2016-03-10 00:00:00"]
-for mmsi, track in tdat.groupby('vessel_A'):
+#tdat = mdat[mdat['timestamp'] == "2016-03-10 00:00:00"]
+for mmsi, track in test.groupby('vessel_A'):
 
     x = track.vessel_A_lon.values
     y = track.vessel_A_lat.values
@@ -184,8 +197,93 @@ for mmsi, track in tdat.groupby('vessel_A'):
     ax.plot(x, y, label=mmsi, transform=ccrs.PlateCarree())
     ax.plot(x, y, 'o', markersize=1, color = 'black', label=mmsi, transform=ccrs.PlateCarree())
 ax.plot(-25.463418, -24.623938, 'o', markersize=10, color = 'red', transform=ccrs.PlateCarree())
-fig
+ax.plot(pdat['lon'], pdat['lat'], label=mmsi, transform=ccrs.PlateCarree())
+ax.plot(pdat['lon'], pdat['lat'], 'o', markersize=1, color = 'red', label=mmsi, transform=ccrs.PlateCarree())
 
-#ax.plot(pdat['lon'], pdat['lat'], label=mmsi, transform=ccrs.PlateCarree())
-#ax.plot(pdat['lon'], pdat['lat'], 'o', markersize=1, color = 'red', label=mmsi, transform=ccrs.PlateCarree())
 
+
+
+# Animated Map
+
+# Patagonia shelf
+lon1 = -77
+lon2 = -22
+lat1 = -58
+lat2 = -23
+
+# Puerto Mardryn Area
+lon1 = -65.925222
+lon2 = -54.418267
+lat1 = -45.145745
+lat2 = -38.147991
+
+# Zoom out Puerto Mardryn Area
+lon1 = -68
+lon2 = -51
+lat1 = -48
+lat2 = -35
+
+#gdat = mdat[mdat['vessel_A'] == 900028621]
+#vessel_b = list(gdat['vessel_B'].unique())
+#vessel_b.append(gdat['vessel_A'].iat[0])
+
+#gdat = mdat[mdat['vessel_B'].isin(vessel_b)]
+
+#gdat = mdat[mdat['timestamp'] == "2016-03-10 13:00:00"]
+
+gdat = mdat
+
+for mmsi, group in gdat.groupby('timestamp'):
+    #lon = group.vessel_A_lon.values[1]
+    #lat = group.vessel_A_lat.values[1]
+    #lon1 = lon - 1
+    #lon2 = lon + 1
+    #lat1 = lat - 1
+    #lat2 = lat + 1
+    
+    # Zoom out Puerto Mardryn Area
+    lon1 = -68
+    lon2 = -51
+    lat1 = -48
+    lat2 = -35
+    
+    my_dpi=96
+    fig = plt.figure(figsize=(2600/my_dpi, 1800/my_dpi), dpi=my_dpi)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.stock_img()
+    #ax.add_feature(cfeature.LAND)
+    #ax.add_feature(cfeature.COASTLINE)
+    #ax.add_feature(states_provinces, edgecolor='gray')
+    ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
+    ax.coastlines()
+    #ax.stock_img()
+
+    VA_x = group.vessel_A_lon.values
+    VA_y = group.vessel_A_lat.values
+    
+    VB_x = group.vessel_B_lon.values
+    VB_y = group.vessel_B_lat.values
+    
+    ax.plot(VB_x, VB_y, 'o', markersize=2, color = 'red', label=mmsi, transform=ccrs.PlateCarree())
+    #ax.plot([VB_x, VA_x], [VB_y,  VA_y], marker = 'o', linewidth=1, markersize=2, color = 'black', label=mmsi, transform=ccrs.PlateCarree())
+    #ax.plot(VA_x, VA_y, 'o', markersize=2, color = 'red', label=mmsi, transform=ccrs.PlateCarree())
+    plt.annotate(f"Timestamp: {group['timestamp'].iat[0]}", xy=(0.22, .85), xycoords='figure fraction', fontsize=20, color='blue')
+    #plt.annotate(f"MMSI: {group['vessel_A'].iat[0]}", xy=(0.7, .1), xycoords='figure fraction', fontsize=20, color='blue') 
+    filepath = f"/home/server/pi/homes/woodilla/Projects/Patagonia-EDA/figures/{group['timestamp'].iat[0]}.jpg"
+    
+    plt.savefig(filepath, dpi=300)
+
+    
+    
+    
+#tdat = mdat[mdat['timestamp'] == "2016-03-10 00:00:00"]
+for mmsi, track in test.groupby('vessel_A'):
+
+    x = track.vessel_A_lon.values
+    y = track.vessel_A_lat.values
+    
+    ax.plot(x, y, label=mmsi, transform=ccrs.PlateCarree())
+    ax.plot(x, y, 'o', markersize=1, color = 'black', label=mmsi, transform=ccrs.PlateCarree())
+ax.plot(-25.463418, -24.623938, 'o', markersize=10, color = 'red', transform=ccrs.PlateCarree())
+ax.plot(pdat['lon'], pdat['lat'], label=mmsi, transform=ccrs.PlateCarree())
+ax.plot(pdat['lon'], pdat['lat'], 'o', markersize=1, color = 'red', label=mmsi, transform=ccrs.PlateCarree())
